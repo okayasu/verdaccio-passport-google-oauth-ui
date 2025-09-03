3b6c002f40ca16fe60cabc784329413818117665
岡安正順 <okayasu@bexide.co.jp>
2025-09-03 17:25:14 +0900

    support node22

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -1,4 +1,7 @@
+import url from "url"
+import path from "path"
 import { pluginKey } from "../constants"
 
-export const publicRoot = __dirname + "/../client"
+const __filename = url.fileURLToPath(new URL('./constants.ts', import.meta.url))
+export const publicRoot = path.resolve(path.dirname(__filename) + "/../client")
 export const staticPath = "/-/static/" + pluginKey
