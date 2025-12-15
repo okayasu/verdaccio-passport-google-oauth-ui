502fcf3f7236bfd77a1f8dd7bc5078da50a0a2aa
岡安正順 <okayasu@bexide.co.jp>
2025-12-15 17:15:01 +0900

    Update: support no https server

--- src/client/plugin/clipboard.ts
+++ src/client/plugin/clipboard.ts
@@ -1,3 +1,11 @@
+import copy from "copy-to-clipboard"
+
+export function copyToClipboard(text: string) {
+  copy(text)
+}
+
+/*
 export async function copyToClipboard(text: string) {
   await navigator.clipboard.writeText(text)
 }
+*/
