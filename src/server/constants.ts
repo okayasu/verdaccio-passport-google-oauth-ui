c43110ab3d0de924c9b969717b8e7e4bcf428fc2
岡安正順 <okayasu@bexide.co.jp>
2026-05-16 18:57:54 +0900

    build: upgrade parcel and adjust runtime dirname resolution

--- src/server/constants.ts
+++ src/server/constants.ts
@@ -1,7 +1,10 @@
-import url from "url"
-import path from "path"
 import { pluginKey } from "../constants"
 
-const __filename = url.fileURLToPath(new URL('./constants.ts', import.meta.url))
-export const publicRoot = path.resolve(path.dirname(__filename) + "/../client")
+// Parcel will replace `__dirname` with the directory of the source file during
+// the build. Using `eval` prevents this static analysis and substitution,
+// allowing us to access the `__dirname` of the compiled output file at runtime
+// to locate the adjacent client assets.
+const fileDirectory = eval("__dirname")
+
+export const publicRoot = fileDirectory + "/../client"
 export const staticPath = "/-/static/" + pluginKey
